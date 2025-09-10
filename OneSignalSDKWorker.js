05b7665f0e68ffc88e04880cd0231e9829d9b27f
Chuz <jesusac1992@gmail.com>
2025-09-10 09:17:35 -0600

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,17 +1,2 @@
 // Quitar código complejo para minimizar conflictos
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
-
-// Agregar event listener para evitar errores de mensajes
-self.addEventListener('message', function(event) {
-  // Simplemente responde a los mensajes para evitar errores
-  if (event.data && event.data.type) {
-    self.clients.matchAll().then(function(clients) {
-      clients.forEach(function(client) {
-        client.postMessage({
-          type: event.data.type + '_response',
-          success: true
-        });
-      });
-    });
-  }
-});
